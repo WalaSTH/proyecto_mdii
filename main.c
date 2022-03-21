f00228833e4cd3fe5da04e1200f4d205fe699f97
Diego Gimenez <d.gimenez0101@gmail.com>
2022-03-20 22:41:55 -0300

    Added gitignore

--- main.c
+++ main.c
@@ -6,23 +6,31 @@
 typedef unsigned int u32;
 
 int main() {
-    char *x = malloc(sizeof(char)*1);
+    char *x = malloc(sizeof(char)*2);
     assert(x!=NULL);
-    u32 y, z;
+    u32 y, z, n;
     FILE *fp;
 
-    unsigned int *ordArray = malloc(sizeof(u32)*3);
+    n = 3;
+    unsigned int *ordArray = malloc(sizeof(u32)*(2*n));
     assert(ordArray!=NULL);
     
+    // Empiezo a leer datos
     fp = fopen("datos.txt", "r");
-
-    for(size_t i = 0; i<3; ++i){
+    for(size_t i = 0; i<n; ++i){
         fscanf(fp, "%c %u %u\n", x, &y, &z);
-        ordArray[i] = y;
-        printf("%s %u %u\n", x, ordArray[i], z);
+        
+        // Forma de indezar ordArray[ancho * fila + columna]
+        ordArray[2*i] = y;
+        ordArray[2*i+1] = z;
     }
     
     fclose(fp);
+    // Termine de leer datos
+
+    for(u32 i=0; i<(2*n); ++i)
+        printf("%u\n", ordArray[i]);
+    
     free(x);
     free(ordArray);
     return 0;
