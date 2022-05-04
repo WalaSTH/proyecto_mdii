1fadeaca8a0beb75bf6719d5f6f8379d97fdd08e
WalaSTH <95602393+WalaSTH@users.noreply.github.com>
2022-05-04 20:50:51 -0300

    Greedy algorithm implemented

--- coloreo.c
+++ coloreo.c
@@ -35,8 +35,7 @@ puntero a un array de u32s que debe tener longitud n = n ́umero de v ́ertices 
 para ese array debe ser hecha dentro de Bipartito, obviamente.
 Si χ(G) ≥ 3, devuelve un puntero a NULL. */
 u32 *Bipartito(Grafo G){
-    u32 lados = G->m_lados;
-    u32 vertices = G->n_vertices;
+    u32 vertices = NumeroDeVertices(G);
     u32 *coloreo = calloc(vertices, sizeof(u32));
     bool bipartito = true;
 
@@ -48,7 +47,7 @@ u32 *Bipartito(Grafo G){
                 free(coloreo);
                 coloreo = NULL;
                 return coloreo;
-            };
+            }
         }
         ++i;
     }
@@ -58,10 +57,57 @@ u32 *Bipartito(Grafo G){
 
 // Salve principe Sheogorath
 u32 Greedy(Grafo G, u32 *Orden, u32 *Coloreo){
-    return 0;
+    u32 nVertices = NumeroDeVertices(G);
+    bool *vertFueColoreado = calloc(nVertices, sizeof(bool)),
+         *colorFueUsado = NULL;
+
+    // Coloreamos el primer vértice
+    u32 maxColor = 0;
+    u32 vertActual = Orden[0];
+    Coloreo[vertActual] = 0;
+    vertFueColoreado[vertActual] = true;
+
+    for(u32 i = 1; i < nVertices; ++i){
+        // Elegimos el vértice actual
+        vertActual = Orden[i];
+        u32 grado = Grado(vertActual, G);
+
+        // Inicializamos el array de colores usados
+        colorFueUsado = calloc(maxColor + 2, sizeof(bool));
+        for(u32 j = 0; j < grado; ++j){
+            u32 jVecino = IndiceONVecino(j, vertActual, G);
+            if(vertFueColoreado[jVecino]){
+                colorFueUsado[Coloreo[jVecino]] = true;
+            }
+        }
+
+        // Buscamos el menor color que no haya sido usado
+        u32 color = 0;
+        while(colorFueUsado[color]){
+            ++color;
+        }
+
+        // Asignamos el color
+        Coloreo[vertActual] = color;
+        vertFueColoreado[vertActual] = true;
+
+        // Actualizamos el color máximo
+        if(maxColor < color){
+            maxColor = color;
+        }
+
+        // Liberamos el array de colores usados
+        free(colorFueUsado);
+        colorFueUsado = NULL;
+    }
+
+    free(vertFueColoreado);
+    vertFueColoreado = NULL;
+
+    return maxColor + 1;
 }
 
-//ordenamiento a partir de clave
+// Ordenamiento a partir de clave
 char OrdenFromKey(u32 n,u32* key,u32* Orden){
     return (char) 0;
 }
